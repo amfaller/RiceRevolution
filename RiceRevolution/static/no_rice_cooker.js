1d631c748cbb1e8042965465c345c253820f738d
Tony Faller <49420290+amfaller@users.noreply.github.com>
2024-04-13 12:15:59 -0400

    Update no_rice_cooker.js

--- RiceRevolution/static/no_rice_cooker.js
+++ RiceRevolution/static/no_rice_cooker.js
@@ -8,3 +8,6 @@ function renderNrcHome() {
     // Render global margins
     functions.renderMargins();
 }
+
+// TODO : Copy infrastructure from Rice Cooker guide to No Rice Cooker guide
+// Or refactor that infrastructure out to functions.js and use it here
